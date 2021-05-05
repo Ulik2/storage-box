a918a2d56f2ea0dd8bfbce8a02c717d2b9f29716
Ulik2 <82815851+Ulik2@users.noreply.github.com>
2021-05-05 23:51:34 +0900

    Add files via upload

---
+++ 2021.05.05./택시매칭.py
@@ -0,0 +1,12 @@
+from random import *
+
+cnt = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if time >= 5 and time <=15:
+        print("[O] {0}번째 손님 (소요시간: {1})".format(i, time))
+        cnt += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간: {1})".format(i, time))
+print("\n")
+print("총 탑승 승객 수: {0}명".format(cnt))
