78abdd85593f6043c2f6283135aa7b5eadce79f9
Ulik2 <82815851+Ulik2@users.noreply.github.com>
2021-05-05 23:52:39 +0900

    Update 택시매칭.py

--- 2021.05.05./택시매칭.py
+++ 2021.05.05./택시매칭.py
@@ -2,7 +2,7 @@ from random import *
 
 cnt = 0
 for i in range(1, 51):
-    time = randrange(5, 51)
+    time = randrange(5, 51) # rnadrange 이걸 통해서 배웠다 활용하자!
     if time >= 5 and time <=15:
         print("[O] {0}번째 손님 (소요시간: {1})".format(i, time))
         cnt += 1
